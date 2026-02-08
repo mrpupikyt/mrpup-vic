a34e637bac6b45c5cf4493ef2dd9a4c3461fc78c
mrpupikyt <mrpupikyt@gmail.com>
2026-02-08 23:38:08 +0200

    Add files via upload

--- MrPupVicious.lua
+++ MrPupVicious.lua
@@ -4,6 +4,24 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jen
 
 local Window = OrionLib:MakeWindow({Name = "MrPup Vicious", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
+function autoNight()
+    local timeString = game.Lighting.TimeOfDay
+    local hour = tonumber(string.sub(timeString, 1, 2))
+    
+    if hour >= 0 and hour < 8 then
+        return true
+    else
+        return false
+    end
+end
+
+task.spawn(function()
+    while true do
+        TabNight.Name = "NightDetect: " .. tostring(autoNight())
+        task.wait(2)
+    end
+end)
+
 else
     return
 end
