b48a1831e97c5a61c845d6dfd662a7eb04572e6c
mrpupikyt <mrpupikyt@gmail.com>
2026-02-08 23:57:53 +0200

    Add files via upload

--- MrPupVicious.lua
+++ MrPupVicious.lua
@@ -2,19 +2,18 @@ if game.PlaceId == 1537690962 then
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "MrPup Vicious", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "MrPup Vicious", HidePremium = false, SaveConfig = true, ConfigFolder = "VicMrpup"})
 
 
 
 local Tab = Window:MakeTab({
 	Name = "Vic",
+	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
 
 
-else
-    return
 end
 
 OrionLib:Init()
